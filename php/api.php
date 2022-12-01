62f165f0988af78d2ad49c1eb08a8fd391d322fc
machih <machih@vmware.com>
2022-12-01 17:29:39 +0900

    Version 1

--- php/api.php
+++ php/api.php
@@ -2,24 +2,21 @@
 header("Content-Type:application/json");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
-$con = mysqli_connect($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASS'],$_ENV['DB_NAME']);
+$con = mysqli_connect(getenv('DB_HOST'),getenv('DB_USERNAME'),getenv('DB_PASSWORD'),getenv('DB_NAME'));
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     die();
 }
 $result = mysqli_query(
-     $con,
-     "SELECT * FROM `data`");
+    $con,
+    "SELECT * FROM `data`");
 if(mysqli_num_rows($result)>0){
-    $row = mysqli_fetch_array($result);
-    response($row);
+    while($row = $result->fetch_assoc()) {
+        $myArray[] = $row;
+    }
+    echo json_encode($myArray);
     mysqli_close($con);
 }else{
     response(NULL, NULL, 200,"No Record Found");
-}
-
-function response($row){
-    $json_response = json_encode($row);
-    echo $json_response;
 }
 ?>
